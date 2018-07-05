5a16830968b60727498baadbcca5e0cb016121b4
p.edwin200294@gmail.com <p.edwin200294@gmail.com>
2018-07-05 07:51:07 +0000

    updated jboss url

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-curl -L -O https://access.redhat.com/cspdownload/62539b312a9759ff899a2bf2d543501e/5b2b4a1b/JBEAP-6.4.0/jboss-eap-6.4.0.zip 
+curl -L -O jboss-eap-6.4.0.zip https://access.cdn.redhat.com/content/origin/files/sha256/27/27a6fd62a8bc4f660970ab282a4bc013934275e47a850a974db6c7d2c62cc50e/jboss-eap-6.4.0.zip?_auth_=1530776720_21f28034f0a6acb0de6d728f7121b48f 
 #git clone https://github.com/EdwinPhilip/jboss_zip.git
 echo "Installation........................"
 mkdir EAP_6.4
